80355e1e69e3f0c648c6e23950f819c5927191c0
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-11-12 00:26:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog 
   def name=(dogs_name)
-    @dogs_name = 
+    @dogs_name = puts "Woof!"
     
   end
   def name
-    @this_dogs_name
+    @dogs_name
   end
   def bark=(dog_bark)
     @this_dog_bark = dog_bark
